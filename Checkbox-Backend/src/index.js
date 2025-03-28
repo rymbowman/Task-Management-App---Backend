48895614ebbc037c6c5256b4e2fc38577be3ccc1
Ryan Bowman <rymbowman@gmail.com>
2025-03-28 09:52:51 -0400

    Add middleware for CORS and JSON parsing

--- Checkbox-Backend/src/index.js
+++ Checkbox-Backend/src/index.js
@@ -6,3 +6,10 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
